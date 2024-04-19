b60a09e0bde1d37e77b3e74e9e1c7dfc803bf02a
John Maksuta <jmaksuta@colostate.edu>
2024-04-19 16:38:08 -0600

    Completed Task 3 Equation class.

--- Assignments/HW-ExtraCredit/cs250/ec/order/Equation.java
+++ Assignments/HW-ExtraCredit/cs250/ec/order/Equation.java
@@ -1,18 +1,35 @@
 package cs250.ec.order;
 
+import java.security.InvalidParameterException;
+
 public class Equation {
+
+    private int x;
+    private int y;
 
     public static void main(String[] args) {
         try {
-            Equation equation = new Equation();
+            if (args.length != 2) {
+                throw new InvalidParameterException("Expects 2 parameters");
+            }
+            Equation equation = new Equation(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            System.out.printf("%.5f\n", equation.perform());
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public Equation() {
+    public Equation(int x, int y) {
         super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public double perform() {
+        double numerator = Math.log10(Math.abs(x * y)) - Math.pow(x, 4);
+        double denominator = Math.sqrt(Math.pow(x * y, 2)) + Math.pow(y, 3) * x;
+        return numerator / denominator;
     }
 
 }
